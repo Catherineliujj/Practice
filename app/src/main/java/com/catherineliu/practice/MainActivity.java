e981443fb76060519a6a943236b5b3bf0925c0f2
Catherine1106 <2291741273@qq.com>
2019-08-04 17:36:42 +0800

    by 1106/20190804

--- app/src/main/java/com/catherineliu/practice/MainActivity.java
+++ app/src/main/java/com/catherineliu/practice/MainActivity.java
@@ -3,6 +3,9 @@ package com.catherineliu.practice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @author Catherine Liu
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
